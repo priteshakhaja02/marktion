206193138db2b2f795dd82b2ff2317c6682c8d7f
Pritesh <priteshakhaja@gmail.com>
2024-02-05 14:59:53 +0530

    Update ai-chat-panel.js (no-context, small-context, large-context)

--- dist/react-components/ai/ai-chat-panel.js
+++ dist/react-components/ai/ai-chat-panel.js
@@ -8,40 +8,39 @@ import { ChatMessages } from './ai-chat-messages';
 import { ChatMenu, ChatMenuKey } from './ai-chat-menu';
 import { insertMessages } from './helper';
 import getConfig from "next/config";
-const APICALL_KEY_OPENAI = 'openai'
-const APICALL_KEY_FILE = 'file'
+
+// const APICALL_KEY_OPENAI = 'openai'
+// const APICALL_KEY_FILE = 'file'
 // const defaultInitialMessages = DEBUG_MESSAGE;
+const NO_CONTEXT = 'no-context'
+const SMALL_CONTEXT = 'small-context'
+const LARGE_CONTEXT = 'large-context'
 const defaultInitialMessages = [];
+
 export function AIChatPanel({ children, gptConfig, selection, ...popoverProps }) {
+
     const inputRef = useRef(null);
-    const buttonRef = useRef(null);
     const [chatMenuOpen, setChatMenuOpen] = useState(false);
-    const [isOpenAiCall, setIsOpenAiCall] = useState(APICALL_KEY_FILE);
+    const [inputEvent, setInputEvent] = useState({});
+    const [key, setKey] = useState(NO_CONTEXT);
+    // const [isOpenAiCall, setIsOpenAiCall] = useState(APICALL_KEY_FILE);
     const chatMenuWrapperRef = useRef(null);
     const { token } = theme.useToken();
     const pm = usePMRenderer();
-    console.log("getCOnfig", buttonRef)
+
+    // console.log("gptConfig",gptConfig)
+
     const { messages, input, isLoading, handleInputChange, handleSubmit, setMessages, stop } = useChat({
-        // api: gptConfig.baseUrl + '?call='+ buttonRef.current,
+        api: gptConfig.baseUrl + '?type='+ key,
         initialMessages: defaultInitialMessages,
-        body: {
-            temperature: 0.7,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0,
-            stream: true,
-            n: 1,
-            model: 'gpt-3.5-turbo'
-        },
         headers: {
-            Authorization: `Bearer ${gptConfig?.apiKey || ''}`
+            Authorization: `Bearer ${gptConfig?.token || ''}`
         },
-        // headers: {
-        //     Authorization: `Bearer ${gptConfig?.token || ''}`
-        // },
         ...gptConfig
     });
+
     const [isComposingInput, setIsComposingInput] = useState(false);
+
     useEffect(() => {
         if (!popoverProps.open) {
             setMessages(defaultInitialMessages);
@@ -53,6 +52,7 @@ export function AIChatPanel({ children, gptConfig, selection, ...popoverProps })
             });
         }
     }, [popoverProps.open]);
+
     useEffect(() => {
         if (!popoverProps.open) {
             setChatMenuOpen(false);
@@ -62,11 +62,13 @@ export function AIChatPanel({ children, gptConfig, selection, ...popoverProps })
             setChatMenuOpen(true);
         }
     }, [messages.length >= 2, popoverProps.open, isLoading]);
+
     useEffect(() => {
         if (!isLoading) {
             inputRef.current?.focus();
         }
     }, [isLoading]);
+
     useEffect(() => {
         if (!popoverProps.open) {
             return;
@@ -82,87 +84,121 @@ export function AIChatPanel({ children, gptConfig, selection, ...popoverProps })
             window.removeEventListener('keydown', onKeydown);
         };
     }, [popoverProps.onOpenChange, popoverProps.open]);
-    const onSubmit = (e) => {
-        console.log(e)
+
+    const onSubmit = (e,key) => {
         if (isComposingInput)
             return;
-        if (e?.shiftKey)
-            return;
-        handleSubmit(e);
+        // if (e?.shiftKey)
+        //     return;
+        setKey(key)
+        setInputEvent(e)
     };
+
+    useEffect(()=>{
+        if (Object.keys(inputEvent)?.length > 0) {
+            handleSubmit(inputEvent);
+        }
+    },[inputEvent])
+
     const onSelectMenu = useCallback(key => {
         if (key === ChatMenuKey.InsertToContent) {
             popoverProps.onOpenChange?.(false);
             insertMessages(pm, messages, selection);
         }
     }, [pm, messages]);
-    const inputEl = (_jsx(Input, { style: { boxShadow: 'none' }, bordered: false, value: input, disabled: isLoading, ref: inputRef, prefix: _jsx(SparklesIcon, { onClick: () => setChatMenuOpen(!chatMenuOpen), style: {
+
+    const inputEl = (_jsx(Input, { style: { boxShadow: 'none' },
+            bordered: false,
+            value: input,
+            disabled: isLoading,
+            ref: inputRef,
+            prefix: _jsx(SparklesIcon, { onClick: () => setChatMenuOpen(!chatMenuOpen), style: {
                 cursor: 'pointer',
                 marginRight: token.marginXS,
                 color: token.purple
-            } }), suffix: (
-                // Added a Fragment to contain both buttons
-                _jsxs(_Fragment, {
-                    children: [
-                        // First Button (SparklesIcon)
-                        _jsx(Button, {
-                            children: "OpenAI",
-                            id: APICALL_KEY_OPENAI,
-                            loading: isLoading,
-                            style: {
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                marginRight: '8px' // Adjust margin as needed
-                            },
-                            icon: _jsx(SparklesIcon, {
-                                width: 16,
-                                height: 16
-                            }),
-                            onClick: (e)=> {
-                                buttonRef.current = APICALL_KEY_OPENAI
-                                setIsOpenAiCall(APICALL_KEY_OPENAI);
-                                onSubmit(e);
-                            }
-                        }),
-                        // Second Button (SendHorizonalIcon)
-                        _jsx(Button, {
-                            children: "File",
-                            loading: isLoading,
-                            id: APICALL_KEY_FILE,
-                            style: {
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'center'
-                            },
-                            icon: _jsx(SendHorizonalIcon, {
-                                width: 16,
-                                height: 16
-                            }),
-                            onClick: (e)=> {
-                                buttonRef.current = APICALL_KEY_FILE
-                                setIsOpenAiCall(APICALL_KEY_FILE);
-                                onSubmit(e);
-                            }
-                        })
-                    ]
-                })
-            ), placeholder: "OpenAI GPT-3 Playground", onChange: handleInputChange, onPressEnter: onSubmit, onCompositionStart: () => setIsComposingInput(true), onCompositionEnd: () => setIsComposingInput(false) }));
+            } }),
+            //  suffix: (
+            //     // Added a Fragment to contain both buttons
+            //     _jsxs(_Fragment, {
+            //         children: [
+            //             // First Button (SparklesIcon)
+            //             _jsx(Button, {
+            //                 children: "OpenAI",
+            //                 id: APICALL_KEY_OPENAI,
+            //                 loading: isLoading,
+            //                 style: {
+            //                     display: 'flex',
+            //                     alignItems: 'center',
+            //                     justifyContent: 'center',
+            //                     marginRight: '8px' // Adjust margin as needed
+            //                 },
+            //                 icon: _jsx(SparklesIcon, {
+            //                     width: 16,
+            //                     height: 16
+            //                 }),
+            //                 onClick: (e)=> {
+            //                     buttonRef.current = APICALL_KEY_OPENAI
+            //                     setIsOpenAiCall(APICALL_KEY_OPENAI);
+            //                     onSubmit(e);
+            //                 }
+            //             }),
+            //             // Second Button (SendHorizonalIcon)
+            //             _jsx(Button, {
+            //                 children: "File",
+            //                 loading: isLoading,
+            //                 id: APICALL_KEY_FILE,
+            //                 style: {
+            //                     display: 'flex',
+            //                     alignItems: 'center',
+            //                     justifyContent: 'center'
+            //                 },
+            //                 icon: _jsx(SendHorizonalIcon, {
+            //                     width: 16,
+            //                     height: 16
+            //                 }),
+            //                 onClick: (e)=> {
+            //                     buttonRef.current = APICALL_KEY_FILE
+            //                     setIsOpenAiCall(APICALL_KEY_FILE);
+            //                     onSubmit(e);
+            //                 }
+            //             })
+            //         ]
+            //     })
+            // ), 
+            placeholder: "OpenAI GPT-3 Playground", 
+            onChange: handleInputChange, 
+            onKeyDown: (e)=>{
+                if(e.metaKey && e.shiftKey && e.code == "Enter"){
+                    onSubmit(e,LARGE_CONTEXT)
+                }else if(e.metaKey && e.code == "Enter"){
+                    onSubmit(e,SMALL_CONTEXT)
+                }else if(e.code == "Enter") {
+                    onSubmit(e,NO_CONTEXT)
+                }
+            }, 
+            onCompositionStart: () => setIsComposingInput(true), 
+            onCompositionEnd: () => setIsComposingInput(false) 
+        }));
+
     const renderInputMode = () => {
         return _jsx("div", { style: { padding: token.paddingXS }, children: inputEl });
     };
+
     const renderChatMode = () => {
         return (_jsxs(_Fragment, { children: [_jsx("div", { style: { borderBottom: `1px solid ${token.colorBorderSecondary}` }, children: _jsx(ChatMessages, { messages: messages }) }), _jsx("div", { ref: chatMenuWrapperRef, style: { position: 'relative' }, children: _jsx(ChatMenu, { open: chatMenuOpen, onSelectMenu: onSelectMenu, getPopupContainer: () => chatMenuWrapperRef.current || document.body, children: _jsx("div", { style: { padding: token.paddingXS }, children: inputEl }) }) })] }));
     };
+
     const content = (_jsx("div", { style: {
             width: 600
         }, onWheel: e => {
             e.stopPropagation();
         }, children: messages.length > 0 ? renderChatMode() : renderInputMode() }));
+
     return (_jsx(Popover, { placement: "bottomLeft", trigger: "click", arrow: false, overlayInnerStyle: {
             padding: 0
         }, ...popoverProps, content: content, children: children }));
 }
+
 function messagesToMarkdown(messages) {
     return messages
         .map(message => {
