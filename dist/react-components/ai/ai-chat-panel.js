bc31fddec380e279efa6ef27926b8a929fc0b137
Pritesh <priteshakhaja@gmail.com>
2024-03-26 11:12:02 +0530

    data streaming and insert option

--- dist/react-components/ai/ai-chat-panel.js
+++ dist/react-components/ai/ai-chat-panel.js
@@ -8,6 +8,7 @@ import { ChatMessages } from './ai-chat-messages';
 import { ChatMenu, ChatMenuKey } from './ai-chat-menu';
 import { insertMessages } from './helper';
 import getConfig from "next/config";
+
 // const APICALL_KEY_OPENAI = 'openai'
 // const APICALL_KEY_FILE = 'file'
 // const defaultInitialMessages = DEBUG_MESSAGE;
@@ -15,30 +16,20 @@ const NO_CONTEXT = 'no-context'
 const SMALL_CONTEXT = 'small-context'
 const LARGE_CONTEXT = 'large-context'
 const defaultInitialMessages = [];
+
 export function AIChatPanel({ children, gptConfig, selection, ...popoverProps }) {
+
     const inputRef = useRef(null);
     const [chatMenuOpen, setChatMenuOpen] = useState(false);
     const [inputEvent, setInputEvent] = useState({});
     const [key, setKey] = useState(NO_CONTEXT);
+    // const [isOpenAiCall, setIsOpenAiCall] = useState(APICALL_KEY_FILE);
     const chatMenuWrapperRef = useRef(null);
     const { token } = theme.useToken();
     const pm = usePMRenderer();
-    useEffect(() => {
-        const selection = window.getSelection();
-        function handleMouseUp() {
-            const selectedText = selection.toString().trim();
-            if (selectedText && !popoverProps.open) {
-              setSelectedText(selectedText);
-            }
-        }
-        window.addEventListener("mouseup", handleMouseUp);
-        window.addEventListener("keyup", handleMouseUp);
-        return () => {
-          window.removeEventListener("mouseup", handleMouseUp);
-          window.removeEventListener("keyup", handleMouseUp);
-        };
-        
-      }, [popoverProps.open]);
+
+    // console.log("gptConfig",gptConfig)
+
     const { messages, input, isLoading, handleInputChange, handleSubmit, setMessages, stop } = useChat({
         api: gptConfig.baseUrl + 'chat/editor-chat?initiative_id='+ gptConfig.initid + '&type='+ key,
         initialMessages: defaultInitialMessages,
@@ -47,13 +38,12 @@ export function AIChatPanel({ children, gptConfig, selection, ...popoverProps })
         },
         ...gptConfig
     });
-    const [selectedText, setSelectedText] = useState("");
-    
+
     const [isComposingInput, setIsComposingInput] = useState(false);
+
     useEffect(() => {
         if (!popoverProps.open) {
             setMessages(defaultInitialMessages);
-            setSelectedText("")
             stop();
         }
         else {
@@ -62,23 +52,23 @@ export function AIChatPanel({ children, gptConfig, selection, ...popoverProps })
             });
         }
     }, [popoverProps.open]);
+
     useEffect(() => {
         if (!popoverProps.open) {
             setChatMenuOpen(false);
-            setSelectedText("");
             return;
         }
         if (messages.length >= 2) {
             setChatMenuOpen(true);
         }
     }, [messages.length >= 2, popoverProps.open, isLoading]);
+
     useEffect(() => {
         if (!isLoading) {
             inputRef.current?.focus();
-              popoverProps.onOpenChange?.(false);
-            insertMessages(pm, messages, selection);
         }
     }, [isLoading]);
+
     useEffect(() => {
         if (!popoverProps.open) {
             return;
@@ -86,7 +76,6 @@ export function AIChatPanel({ children, gptConfig, selection, ...popoverProps })
         const onKeydown = (e) => {
             if (e.key === 'Escape') {
                 popoverProps.onOpenChange?.(false);
-                setSelectedText("");
                 pm.focus();
             }
         };
@@ -94,7 +83,8 @@ export function AIChatPanel({ children, gptConfig, selection, ...popoverProps })
         return () => {
             window.removeEventListener('keydown', onKeydown);
         };
-    }, [popoverProps.onOpenChange, popoverProps.open , selectedText]);
+    }, [popoverProps.onOpenChange, popoverProps.open]);
+
     const onSubmit = (e,key) => {
         if (isComposingInput)
             return;
@@ -103,19 +93,27 @@ export function AIChatPanel({ children, gptConfig, selection, ...popoverProps })
         setKey(key)
         setInputEvent(e)
     };
+
     useEffect(()=>{
         if (Object.keys(inputEvent)?.length > 0) {
-          handleSubmit(inputEvent);
+            handleSubmit(inputEvent);
         }
     },[inputEvent])
-    const inputEl = (_jsx(Input, { style: { boxShadow: '0px' },
+
+    const onSelectMenu = useCallback(key => {
+        if (key === ChatMenuKey.InsertToContent) {
+            popoverProps.onOpenChange?.(false);
+            insertMessages(pm, messages, selection);
+        }
+    }, [pm, messages]);
+
+    const inputEl = (_jsx(Input, { style: { boxShadow: 'none' },
             bordered: false,
-            value: isLoading ? "Generating..." :selectedText,
+            value: input,
             disabled: isLoading,
             ref: inputRef,
-            prefix: isLoading ? "" : _jsx(SparklesIcon, { onClick: () => setChatMenuOpen(!chatMenuOpen), style: {
+            prefix: _jsx(SparklesIcon, { onClick: () => setChatMenuOpen(!chatMenuOpen), style: {
                 cursor: 'pointer',
-                color:"#FBBF24",
                 marginRight: token.marginXS,
                 color: token.purple
             } }),
@@ -130,42 +128,55 @@ export function AIChatPanel({ children, gptConfig, selection, ...popoverProps })
                                 display: 'flex',
                                 alignItems: 'center',
                                 justifyContent: 'center',
-                                border : '0px solid transparent',
-                                boxShadow: '0px' // Adding shadow none
+                                border : '0px solid transparent'
                             },
                         })
                     ]
                 })
             ), 
-            placeholder:isLoading ? "Generate...." : "Ask AI to write anything", 
-            disabled : isLoading,
-            onChange:(e) => {handleInputChange(e);setSelectedText(e.target.value)}, 
+            placeholder:isLoading ? "Generating..." : "Ask AI to write anything",
+            onChange: handleInputChange, 
             onKeyDown: (e)=>{
                 const isCmdKey =  e.ctrlKey || (e?.metaKey && navigator?.platform?.toUpperCase()?.indexOf("MAC") >= 0);
-                if(isCmdKey && e.shiftKey && e.code == "Enter"){
+                const isEnter =  e.code == "Enter" || e.code == "NumpadEnter";
+                if(isCmdKey && e.shiftKey && isEnter){
                     onSubmit(e,LARGE_CONTEXT)
-                }else if(isCmdKey && e.code == "Enter"){
+                }else if(isCmdKey && isEnter){
                     onSubmit(e,SMALL_CONTEXT)
-                }else if(e.code == "Enter") {
+                }else if(isEnter) {
                     onSubmit(e,NO_CONTEXT)
                 }
             }, 
             onCompositionStart: () => setIsComposingInput(true), 
             onCompositionEnd: () => setIsComposingInput(false) 
         }));
+
     const renderInputMode = () => {
         return _jsx("div", { style: { padding: token.paddingXS }, children: inputEl });
     };
+
     const renderChatMode = () => {
-        return (_jsxs(_Fragment, { children: [_jsx("div", { style: { borderBottom: `0px solid ${token.colorBorderSecondary}` } }), _jsx("div", { ref: chatMenuWrapperRef, style: { position: 'relative' }, children: _jsx(ChatMenu, {children: _jsx("div", { children: inputEl }) }) })] }));
+        return (_jsxs(_Fragment, { children: [_jsx("div", { style: { borderBottom: `1px solid ${token.colorBorderSecondary}` }, children: _jsx(ChatMessages, { messages: messages }) }), _jsx("div", { ref: chatMenuWrapperRef, style: { position: 'relative' }, children: _jsx(ChatMenu, { open: chatMenuOpen, onSelectMenu: onSelectMenu, getPopupContainer: () => chatMenuWrapperRef.current || document.body, children: _jsx("div", { style: { padding: token.paddingXS }, children: inputEl }) }) })] }));
     };
+
     const content = (_jsx("div", { style: {
-            width: isLoading ? 187 : 600
+            width: 600
         }, onWheel: e => {
             e.stopPropagation();
         }, children: messages.length > 0 ? renderChatMode() : renderInputMode() }));
-    return (_jsx(Popover, { placement: "bottomLeft", trigger:isLoading ? "" :"click", arrow: false, overlayInnerStyle: {
-            padding: 0,
- boxShadow : isLoading ? "none" :""
+
+    return (_jsx(Popover, { placement: "bottomLeft", trigger: "click", arrow: false, overlayInnerStyle: {
+            padding: 0
         }, ...popoverProps, content: content, children: children }));
+}
+
+function messagesToMarkdown(messages) {
+    return messages
+        .map(message => {
+        if (message.role === 'assistant') {
+            return message.content;
+        }
+        return `**Q: ${message.content}**`;
+    })
+        .join('\n\n');
 }
